e8aa209f0ae1a70cf2410ad8517c744215fe7a7b
qeeqez <qeeqez@gmail.com>
2016-06-03 02:23:38 +0300

    Rearranged code Tests re-created

--- src/by/bsu/parallel/main/Main.java
+++ src/by/bsu/parallel/main/Main.java
@@ -6,173 +6,140 @@ import by.bsu.parallel.algorithms.MaxLineElement;
 import by.bsu.parallel.algorithms.ScalarVectorMultiply;
 import by.bsu.parallel.core.WorkController;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
 
     public static final int SIZE = 500;
-    public static double[] result = new double[SIZE];
+    public static final int testsNumber = 10;
+    public static final int threadNumber = 4;
 
+
+    public static double[] result = new double[SIZE];
     public static double[][] mulResult = new double[SIZE][SIZE];
+
+    public static long[] timeInit = new long[threadNumber];
+    public static long[] timeMatrixMultiply = new long[threadNumber];
+    public static long[] timeMaxLineElement = new long[threadNumber];
+    public static long[] timeScalarVectorMultiply = new long[threadNumber];
+    public static long timeStart;
+    public static long timeEnd;
 
     public static void main(String[] args) {
         Integer[][] matrix = new Integer[SIZE][SIZE];
         Integer[][] matrix2 = new Integer[SIZE][SIZE];
-
         Random rand = new Random();
+        System.err.println("Benchmarking...");
+        for (int q = 0; q < testsNumber; q++) { //количество тестов
 
-        System.out.println("Initializing matrix...");
-        long startInit = System.nanoTime();
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                matrix[i][j] = rand.nextInt();
-            }
-        }
-        long finishInit = System.nanoTime();
-        System.out.println("POSLEDOV " + (finishInit - startInit));
-
-
-        long start4Init = System.nanoTime();
-        WorkController workControllerInit = new WorkController(4);
-        workControllerInit.doWorkMatrix(new InitializeMatrix(), matrix2);
-        long finish4Init = System.nanoTime();
-        System.out.println("2 POTOKA " + (finish4Init - start4Init));
-
-        System.out.println("Starting benchmark...");
-        System.out.println();
-
-        System.out.println("-=-=-Multiplication-=-=-");
-        long start4 = System.nanoTime();
-        for (int i = 0; i < mulResult.length; i++) { // aRow
-            for (int j = 0; j < mulResult.length; j++) { // bColumn
-                for (int k = 0; k < mulResult.length; k++) { // aColumn
-                    mulResult[i][j] += matrix[i][k] * matrix2[k][j];
+            timeStart = System.nanoTime();
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    matrix[i][j] = rand.nextInt();
                 }
             }
-        }
-        long finish4 = System.nanoTime();
-        System.out.println("POSLEDOV " + (finish4 - start4));
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                mulResult[i][j] = 0;
+            timeEnd = System.nanoTime();
+            timeInit[0] += timeEnd - timeStart;
+
+            for (int i = 2; i <= threadNumber; i++) {
+                timeStart = System.nanoTime();
+                WorkController workController = new WorkController(i);
+                workController.doWorkMatrix(new InitializeMatrix(), matrix2);
+                timeEnd = System.nanoTime();
+                timeInit[i - 1] += timeEnd - timeStart;
             }
-        }
-        long start4Mult = System.nanoTime();
-        WorkController workControllerMult = new WorkController(2);
-        workControllerMult.doWorkMatrix(new MatrixMultiply(), matrix, matrix2);
-        long finish4Mult = System.nanoTime();
-        System.out.println("2 POTOKA " + (finish4Mult - start4Mult));
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                mulResult[i][j] = 0;
+
+            timeStart = System.nanoTime();
+            for (int i = 0; i < mulResult.length; i++) { // aRow
+                for (int j = 0; j < mulResult.length; j++) { // bColumn
+                    for (int k = 0; k < mulResult.length; k++) { // aColumn
+                        mulResult[i][j] += matrix[i][k] * matrix2[k][j];
+                    }
+                }
             }
+            timeEnd = System.nanoTime();
+            timeMatrixMultiply[0] += timeEnd - timeStart;
+
+            for (int i = 2; i <= threadNumber; i++) {
+                for (double[] row : mulResult)
+                    Arrays.fill(row, 0.0);
+                timeStart = System.nanoTime();
+                WorkController workController = new WorkController(i);
+                workController.doWorkMatrix(new MatrixMultiply(), matrix, matrix2);
+                timeEnd = System.nanoTime();
+                timeMatrixMultiply[i - 1] += timeEnd - timeStart;
+            }
+
+
+            ///////////////////////////////////////////////////////////////////
+            timeStart = System.nanoTime();
+            double max;
+            for (int i = 0; i < SIZE; i++) {
+                max = Double.MIN_VALUE;
+                for (int j = 0; j < SIZE; j++) {
+                    if (max < matrix[i][j]) {
+                        max = matrix[i][j];
+                    }
+                }
+                result[i] = max;
+            }
+            timeEnd = System.nanoTime();
+            timeMaxLineElement[0] += timeEnd - timeStart;
+
+            for (int i = 2; i <= threadNumber; i++) {
+                Arrays.fill(result, 0.0);
+                timeStart = System.nanoTime();
+                WorkController workController = new WorkController(2);
+                workController.doWorkVector(new MaxLineElement(), matrix);
+                timeEnd = System.nanoTime();
+                timeMaxLineElement[i - 1] += timeEnd - timeStart;
+            }
+
+            ///////////////////////////////////////////////////////////
+            Arrays.fill(result, 0.0);
+            timeStart = System.nanoTime();
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    result[i] += matrix[i][j] * matrix2[i][j];
+                }
+            }
+            timeEnd = System.nanoTime();
+            timeScalarVectorMultiply[0] += timeEnd - timeStart;
+
+            for (int i = 2; i <= threadNumber; i++) {
+                Arrays.fill(result, 0.0);
+                timeStart = System.nanoTime();
+                WorkController workController = new WorkController(2);
+                workController.doWorkMatrix(new ScalarVectorMultiply(), matrix, matrix2);
+                timeEnd = System.nanoTime();
+                timeScalarVectorMultiply[i - 1] += timeEnd - timeStart;
+            }
+            System.err.println("Passed " + (q + 1) + " test...");
         }
 
-        long start4Mult3 = System.nanoTime();
-        WorkController workControllerMult3 = new WorkController(3);
-        workControllerMult3.doWorkMatrix(new MatrixMultiply(), matrix, matrix2);
-        long finish4Mult3 = System.nanoTime();
-        System.out.println("3 POTOKA " + (finish4Mult3 - start4Mult3));
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                mulResult[i][j] = 0;
-            }
+        System.out.println();
+        System.out.println("Matrix size: " + SIZE);
+        System.out.println("=====================");
+        System.out.println();
+        System.out.println("-=-=-Matrix Initialisation-=-=-");
+        for (int i = 0; i < threadNumber; i++) {
+            System.out.println((i + 1) + " Threaded: " + (timeInit[i] / testsNumber));
         }
-
-        long start4Mult2 = System.nanoTime();
-        WorkController workControllerMult2 = new WorkController(4);
-        workControllerMult2.doWorkMatrix(new MatrixMultiply(), matrix, matrix2);
-        long finish4Mult2 = System.nanoTime();
-        System.out.println("4 POTOKA " + (finish4Mult2 - start4Mult2));
-
-        ///////////////////////////////////////////////////////////////////
+        System.out.println();
+        System.out.println("-=-=-Matrix Multiplication-=-=-");
+        for (int i = 0; i < threadNumber; i++) {
+            System.out.println((i + 1) + " Threaded: " + (timeMatrixMultiply[i] / testsNumber));
+        }
         System.out.println();
         System.out.println("-=-=-Max Line Element-=-=-");
-        long start2 = System.nanoTime();
-        double max;
-        for (int i = 0; i < SIZE; i++) {
-            max = Double.MIN_VALUE;
-            for (int j = 0; j < SIZE; j++) {
-                if (max < matrix[i][j]) {
-                    max = matrix[i][j];
-                }
-            }
-            result[i] = max;
+        for (int i = 0; i < threadNumber; i++) {
+            System.out.println((i + 1) + " Threaded: " + (timeMaxLineElement[i] / testsNumber));
         }
-        long finish2 = System.nanoTime();
-        System.out.println("POSLEDOV " + (finish2 - start2));
-
-        long start = System.nanoTime();
-        WorkController workController = new WorkController(2);
-        workController.doWorkVector(new MaxLineElement(), matrix);
-        long finish = System.nanoTime();
-        System.out.println("2 POTOKA " + (finish - start));
-        for (int i = 0; i < result.length; i++) {
-            result[i] = 0;
-        }
-
-        long start3 = System.nanoTime();
-        WorkController workController3 = new WorkController(3);
-        workController3.doWorkVector(new MaxLineElement(), matrix);
-        long finish3 = System.nanoTime();
-        System.out.println("3 POTOKA " + (finish3 - start3));
-        for (int i = 0; i < result.length; i++) {
-            result[i] = 0;
-        }
-
-        long start45 = System.nanoTime();
-        WorkController workController4 = new WorkController(3);
-        workController4.doWorkVector(new MaxLineElement(), matrix);
-        long finish45 = System.nanoTime();
-        System.out.println("4 POTOKA " + (finish45 - start45));
-        for (int i = 0; i < result.length; i++) {
-            result[i] = 0;
-        }
-
-        ///////////////////////////////////////////////////////////
         System.out.println();
         System.out.println("-=-=-Scalar Vector Multiply-=-=-");
-        long startOdno = System.nanoTime();
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                result[i] += matrix[i][j] * matrix2[i][j];
-            }
+        for (int i = 0; i < threadNumber; i++) {
+            System.out.println((i + 1) + " Threaded: " + (timeScalarVectorMultiply[i] / testsNumber));
         }
-        long finishOdno = System.nanoTime();
-        System.out.println("POSLEDOV " + (finishOdno - startOdno));
-
-        long startZero = System.nanoTime();
-        WorkController workControllerz = new WorkController(2);
-        ScalarVectorMultiply scalarVectorMultiply = new ScalarVectorMultiply();
-        workControllerz.doWorkMatrix(scalarVectorMultiply, matrix, matrix2);
-        long finishZero = System.nanoTime();
-        System.out.println("2 POTOKA " + (finishZero - startZero));
-        for (int i = 0; i < result.length; i++) {
-            result[i] = 0;
-        }
-
-        long startOne = System.nanoTime();
-        WorkController workControllerOne = new WorkController(3);
-        workControllerOne.doWorkMatrix(scalarVectorMultiply, matrix, matrix2);
-        long finishOne = System.nanoTime();
-        System.out.println("3 POTOKA " + (finishOne - startOne));
-        for (int i = 0; i < result.length; i++) {
-            result[i] = 0;
-        }
-
-        long startTwo = System.nanoTime();
-        WorkController workControllerTwo = new WorkController(4);
-        workControllerTwo.doWorkMatrix(scalarVectorMultiply, matrix, matrix2);
-        long finishTwo = System.nanoTime();
-        System.out.println("4 POTOKA " + (finishTwo - startTwo));
-
-        System.out.println();
-        System.out.println("=======================");
-        for (int i = 0; i < 5; i++) {
-            System.out.println(i + " " + result[i]);
-        }
-        System.out.println("lenght: " + mulResult.length);
-        System.out.println(mulResult[5][10]);
-
     }
 }
